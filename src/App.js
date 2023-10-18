a9985984cea77d65ec5126eff7c562b45d34aeee
olgaodc <olgaodincova@gmail.com>
2023-10-18 21:43:17 +0300

    Snow particles animation added and readme updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
 
 function App() {
+  const init = async (engine) => {
+    await loadFull(engine);
+  }
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
+      <Particles
+        options={{
+          particles: {
+            color: {
+              value: '#fff'
+            },
+            number: {
+              value: 150
+            },
+            opacity: {
+              value: {
+                min: 0.3,
+                max: 1
+              }
+            },
+            shape: {
+              type: 'circle'
+            },
+            size: {
+              value: {
+                min: 1,
+                max: 5
+              }
+            },
+            move: {
+              direction: 'bottom',
+              enable: true,
+              speed: {
+                min: 2,
+                max: 4
+              },
+              straight: false
+            }
+          }
+        }} init={init} />
     </BrowserRouter>
   );
 }
